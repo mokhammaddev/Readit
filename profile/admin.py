38149b8f6c8dc6ccd60b0274e198cdef5181d602
muhammad <muhammadtoychiboyev1@gmail.com>
2023-08-30 12:05:14 +0500

    profile

--- profile/admin.py
+++ profile/admin.py
@@ -1,10 +1,10 @@
-# from django.contrib import admin
-# from .models import Profile
-#
-#
-# class ProfileAdmin(admin.ModelAdmin):
-#     list_display = ['id', 'user']
-#     search_fields = ['user__username', 'user__first_name']
-#
-#
-# admin.site.register(Profile, ProfileAdmin)
+from django.contrib import admin
+from .models import Profile
+
+
+class ProfileAdmin(admin.ModelAdmin):
+    list_display = ['id', 'user']
+    search_fields = ['user__username', 'user__first_name']
+
+
+admin.site.register(Profile, ProfileAdmin)
