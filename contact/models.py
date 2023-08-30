38eb980d402bcbd96ebde3bc806da9e04eb1d971
muhammad <muhammadtoychiboyev1@gmail.com>
2023-08-30 12:04:13 +0500

    contact

--- contact/models.py
+++ contact/models.py
@@ -1,13 +1,13 @@
-# from django.db import models
-#
-#
-# class Contact(models.Model):
-#     name = models.CharField(max_length=222)
-#     email = models.EmailField()
-#     subject = models.CharField(max_length=222)
-#     message = models.TextField()
-#     created_date = models.DateTimeField(auto_now_add=True, null=True)
-#
-#     def __str__(self):
-#         return self.name
-#
+from django.db import models
+
+
+class Contact(models.Model):
+    name = models.CharField(max_length=222)
+    email = models.EmailField()
+    subject = models.CharField(max_length=222)
+    message = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True, null=True)
+
+    def __str__(self):
+        return self.name
+
